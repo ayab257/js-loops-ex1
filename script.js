69648da06c209c6e22bbc5b9baee0d9615406f96
ayab257 <ayab257@gmail.com>
2016-08-24 23:54:11 +0300

    first

---
+++ script.js
@@ -0,0 +1,47 @@
+// for (var i=120; i < 141; i++){
+	// document.write(" " + i);
+// }
+
+// var i, j;
+    // for(i = 0;i < 5; i++){
+      // for(j = 0;j <= i; j++){
+        // document.write('*');
+      // }
+      // document.write('<br/>');
+   // }
+   
+// var student= [
+	// aya={
+		// height: 1.60,
+	// },
+	// diana={
+		// height: 1.70,
+	// },
+	// mazen={
+		// height: 1.80,
+	// },
+// 	
+// ];
+// 
+// for(var height in student){
+	 // console.log(height, student[height]);
+// }
+
+var student= [
+	aya={
+		height: 1.60,
+	},
+	diana={
+		height: 1.70,
+	},
+	mazen={
+		height: 1.80,
+	},
+	
+];
+
+for(var i=0; i<4; i++){
+	 student[i].height= 175;
+	 console.log(student);
+	}
+
